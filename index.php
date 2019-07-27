9a2e10c7cca5bcc5894837c400bdb2d3a33f2546
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-27 13:19:22 -0400

    navbar testing

--- index.php
+++ index.php
@@ -5,9 +5,28 @@
 		<link type="text/css" href="css/bootstrap.min.css" rel="stylesheet">
 		<link type="text/css" href="css/bootstrap-table.css" rel="stylesheet">
 		<link type="text/css" href="css/font-awesome.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
- 
+
+
 <body>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
 
 <div class="container">
 	<div class="col-md-12">
