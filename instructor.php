ba2123e69694dc7b23885762754434ec64badd16
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-27 15:45:35 -0400

    fix

--- instructor.php
+++ instructor.php
@@ -4,15 +4,15 @@ require 'db.php';
 $instructorSql = "SELECT * FROM Instructor";
 $instructorResult = mysqli_query($conn, $instructorSql) or die(mysqli_error($conn));
 $insVal = array();
-$i = 1;
-while ($rowList = mysqli_fetch_array($instructorResult)) {
+$j = 1;
+while ($rowList2 = mysqli_fetch_array($instructorResult)) {
     $ins = array(
-        'instructorID' => $rowList['instructorID'],
-        'firstName' => $rowList['firstName'],
-        'lastName' => $rowList['lastName']
+        'instructorID' => $rowList2['instructorID'],
+        'firstName' => $rowList2['firstName'],
+        'lastName' => $rowList2['lastName']
     );
     array_push($insVal, $ins);
-    $i++;
+    $j++;
 }
 
 echo json_encode($ins);
