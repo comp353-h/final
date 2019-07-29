68cf782a2d10d8850769754c23f4a490baac2260
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-29 16:20:21 -0400

    phone

--- runquery.php
+++ runquery.php
@@ -19,6 +19,7 @@ if(isset($_POST['RUNQUERY'])){
 <head>
     <title>Search</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
