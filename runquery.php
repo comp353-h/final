c3183066065bad2956f472379b6ac6da7477624e
Raghav Sharda <raghavsharda96@gmail.com>
2019-07-29 17:23:15 -0400

    remove faulty css

--- runquery.php
+++ runquery.php
@@ -20,7 +20,6 @@ if(isset($_POST['RUNQUERY'])){
     <title>Search</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="style.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
