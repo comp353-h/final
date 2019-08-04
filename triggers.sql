e9b9532ad8ab479fa378165af7691e794cf9eb44
Raghav Sharda <raghavsharda96@gmail.com>
2019-08-04 19:57:41 -0400

    Add minimum grade trigger

--- triggers.sql
+++ triggers.sql
@@ -2,6 +2,8 @@ DROP TRIGGER IF EXISTS ConferenceRoomTrigger;
 DROP TRIGGER IF EXISTS OfficeTrigger;
 DROP TRIGGER IF EXISTS LabTrigger;
 DROP TRIGGER IF EXISTS ClassroomTrigger;
+DROP TRIGGER IF EXISTS INSERT_CHECK_prereq;
+DROP TRIGGER IF EXISTS UPDATE_CHECK_prereq;
 
 DELIMITER $$
 CREATE TRIGGER ClassroomTrigger BEFORE INSERT ON Classroom
@@ -30,3 +32,81 @@ CREATE TRIGGER ConferenceRoomTrigger BEFORE INSERT ON ConferenceRoom
         INSERT INTO Room ( roomID, buildingID ) VALUES ( NEW.conferenceRoomID, NEW.buildingID );
     END$$
 DELIMITER ;
+
+
+DELIMITER $$
+CREATE TRIGGER INSERT_CHECK_prereq BEFORE INSERT ON StudentCourses
+FOR EACH ROW
+BEGIN
+DECLARE abc varchar(8);
+DECLARE checkfirstEntry INT(1);
+SET abc = (
+SELECT 
+    sc2.grade
+FROM
+    (SELECT 
+        c.prerequisite
+    FROM
+        Course c
+    WHERE
+        c.courseID = NEW.courseID) tabc
+        JOIN
+    StudentCourses sc2 ON (sc2.courseID = tabc.prerequisite)
+                    );
+                    
+SET checkfirstEntry = (SELECT 
+    COUNT(sc.courseID)
+FROM StudentCourses sc
+	WHERE sc.studentID = 1);
+    
+  IF abc IS NULL AND checkfirstEntry > 1
+THEN
+	SIGNAL SQLSTATE '45000'
+           SET MESSAGE_TEXT = 'Prerequisites for this course is not comepleted yet';
+                   END IF;
+
+  IF abc = 'F'
+THEN
+	SIGNAL SQLSTATE '45000'
+           SET MESSAGE_TEXT = 'You failed in prerequisite course';           
+    END IF;
+END;$$
+
+
+DELIMITER $$
+CREATE TRIGGER UPDATE_CHECK_prereq BEFORE UPDATE ON StudentCourses
+FOR EACH ROW
+BEGIN
+DECLARE abc varchar(8);
+DECLARE checkfirstEntry INT(1);
+SET abc = (
+SELECT 
+    sc2.grade
+FROM
+    (SELECT 
+        c.prerequisite
+    FROM
+        Course c
+    WHERE
+        c.courseID = NEW.courseID) tabc
+        JOIN
+    StudentCourses sc2 ON (sc2.courseID = tabc.prerequisite)
+                    );
+                    
+SET checkfirstEntry = (SELECT 
+    COUNT(sc.courseID)
+FROM StudentCourses sc
+	WHERE sc.studentID = 1);
+    
+  IF abc IS NULL AND checkfirstEntry > 1
+THEN
+	SIGNAL SQLSTATE '45000'
+           SET MESSAGE_TEXT = 'Prerequisites for this course is not comepleted yet';
+                   END IF;
+
+  IF abc = 'F'
+THEN
+	SIGNAL SQLSTATE '45000'
+           SET MESSAGE_TEXT = 'You failed in prerequisite course';           
+    END IF;
+END;$$
