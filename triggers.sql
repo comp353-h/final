436e9022862e81d04934f21274d113b956b93181
Wmbat <wmbat@protonmail.com>
2019-08-05 11:58:09 -0400

    test

--- triggers.sql
+++ triggers.sql
@@ -1,31 +1,31 @@
-DROP TRIGGER IF EXISTS ConferenceRoomTrigger;
-DROP TRIGGER IF EXISTS OfficeTrigger;
-DROP TRIGGER IF EXISTS LabTrigger;
-DROP TRIGGER IF EXISTS ClassroomTrigger;
+DROP TRIGGER IF EXISTS ConferenceRoomInsertTrigger;
+DROP TRIGGER IF EXISTS OfficeInsertTrigger;
+DROP TRIGGER IF EXISTS LabInsertTrigger;
+DROP TRIGGER IF EXISTS ClassroomInsertTrigger;
 
 DELIMITER $$
-CREATE TRIGGER ClassroomTrigger BEFORE INSERT ON Classroom
+CREATE TRIGGER ClassroomInsertTrigger BEFORE INSERT ON Classroom
     FOR EACH ROW BEGIN
         INSERT INTO Room ( roomID, buildingID ) VALUES ( NEW.classroomID, NEW.buildingID );
     END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE TRIGGER LabTrigger BEFORE INSERT ON Lab
+CREATE TRIGGER LabInsertTrigger BEFORE INSERT ON Lab
     FOR EACH ROW BEGIN
         INSERT INTO Room ( roomID, buildingID ) VALUES ( NEW.labID, NEW.buildingID );
     END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE TRIGGER OfficeTrigger BEFORE INSERT ON Office
+CREATE TRIGGER OfficeInsertTrigger BEFORE INSERT ON Office
     FOR EACH ROW BEGIN
         INSERT INTO Room ( roomID, buildingID ) VALUES ( NEW.officeID, NEW.buildingID );
     END$$
 DELIMITER ;
 
 DELIMITER $$
-CREATE TRIGGER ConferenceRoomTrigger BEFORE INSERT ON ConferenceRoom
+CREATE TRIGGER ConferenceRoomInsertTrigger BEFORE INSERT ON ConferenceRoom
     FOR EACH ROW BEGIN
         INSERT INTO Room ( roomID, buildingID ) VALUES ( NEW.conferenceRoomID, NEW.buildingID );
     END$$
